d33e5fd49ebfd9bcb9a29cc3339dc8581b45e1db
Mario Serano <marioserano55@gmail.com>
2023-05-24 23:39:11 +0700

    restore old project structure

--- src/main.py
+++ src/main.py
@@ -1,13 +1,17 @@
-import uiautomator2 as u2
-import uiautomator2.ext.perf as perf
+from ppadb.client import Client as AdbClient
+from ppadb.device import Device
 
-package_name = 'com.android.chrome'
+"""
+User defined parameters, you can modify these
+"""
+TESTING_PACKAGE = 'com.example.passenger_client'  # package that we are testing
+SECONDS = 25  # length of the test in seconds
 
-
-def main():
-    d = u2.connect('RR8NA05A0FP')
-    print(d.info)
-
+"""
+Do not modify these
+"""
+CONNECTED_ADB_CLIENT = AdbClient(host="127.0.0.1", port=5037)
+CONNECTED_DEVICE = CONNECTED_ADB_CLIENT.devices()[0]
 
 if __name__ == "__main__":
-    main()
+    pass
